413823dbfd912a549d7eb38e073cf87e9fea7175
Martin Cortes <martin.cortes@gmail.com>
2019-11-20 16:00:36 -0600

    Primer commit

---
+++ sw.js
@@ -0,0 +1,83 @@
+// imports de repositorio
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+
+//Todo lo que es necesario para mi aplicación
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
+];
+
+
+self.addEventListener('install', e=> {
+
+    const cacheStatic = caches.open ( STATIC_CACHE ).then( cache => 
+        cache.addAll(APP_SHELL));
+
+    const cacheInmutable = caches.open ( INMUTABLE_CACHE ).then( cache => 
+        cache.addAll(APP_SHELL_INMUTABLE));
+
+    e.waitUntil( Promise.all( [cacheStatic, cacheInmutable ]));
+
+});
+
+
+self.addEventListener('activate', e => {
+
+    const activacion = caches.keys().then( keys => { //Recorro
+
+        keys.forEach( key =>{
+
+            if(key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+        });
+
+    });
+
+    e.waitUntil(  activacion  );
+
+});
+
+
+self.addEventListener('fetch', e => {
+
+    const respuesta = caches.match( e.request ).then( res => {
+        if (res) {
+            return res;
+        }else{
+    
+            console.log( e.request.url );
+            //Si llegué aca tengo que ir a buscar ese recurso a internet
+            return fetch( e.request ).then (newResp =>{
+
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newResp);
+
+            });
+        }
+    });
+
+    e.respondWith( respuesta );
+
+});
